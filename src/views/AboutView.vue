8b08e935e3a35d97e2de2328855568e123f389ce
renerrr <r.rodriguez@oskberlin.com>
2023-07-18 16:07:31 +0200

    app working - need to organize code

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -3,7 +3,11 @@ import { Loader } from '@googlemaps/js-api-loader'
 export default {
   data() {
     return {
-      listItems: []
+      listItems: [],
+      map: '',
+      service: '',
+      infoWindow: '',
+      myLatLng: { lat: 52.524697, lng: 13.442576 },
     }
   },
   methods: {
@@ -11,108 +15,150 @@ export default {
       const loader = new Loader({
         apiKey: 'AIzaSyCRiINOpnvk-p9m7AJIhxPTbuAQif7L4n4',
         version: '',
-        libraries: ['places']
+        libraries: ['places', 'marker']
       })
 
       try {
         const google = await loader.load()
-        const mapOptions = {
-          center: {
-            lat: 0,
-            lng: 0
-          },
-          zoom: 4
-        }
-
-        var coco = new google.maps.LatLng(41.40807945494184, 2.1554292820665517)
-        // const map = new google.maps.Map(document.getElementById('map'), {
-        //   center: coco,
-        //   zoom: 15
-        // })
-        var request = {
+        let coco = new google.maps.LatLng(this.myLatLng.lat, this.myLatLng.lng)
+        this.map = new google.maps.Map(document.getElementById('map'), {
+          center: coco,
+          zoom: 15,
+          mapTypeId: google.maps.MapTypeId.ROADMAP
+        })
+        this.infoWindow = new google.maps.InfoWindow({
+          content: 'Click the map to get Lat/Lng!',
+          position: this.myLatlng
+        })
+        let request = {
           location: coco,
           radius: '1500',
-          type: [''],
+          type: ['']
         }
         let place_request = {
-          placeId: "", 
+          placeId: '',
           fields: ['business_status', 'name', 'type', 'website', 'formatted_phone_number']
         }
 
-        var service = new google.maps.places.PlacesService(map)
-        service.nearbySearch(request, callback);
-        function callback(results, status) {
-          console.log(results);
+        this.service = new google.maps.places.PlacesService(this.map)
+        const callback = (results, status) => {
           for (let i = 0; i < results.length; i++) {
-            place_request.placeId = results[i].place_id;
-            console.log(place_request);
-            service.getDetails(place_request, callbackPlace);
-
+            place_request.placeId = results[i].place_id
+            this.service.getDetails(place_request, callbackPlace)
           }
 
-          // if (status == google.maps.places.PlacesServiceStatus.OK) {
-          //   for (var i = 0; i < results.length; i++) {
-          //     createMarker(results[i])
-          //   }
-          // }
+          if (status == google.maps.places.PlacesServiceStatus.OK) {
+            for (var i = 0; i < results.length; i++) {
+              createMarker(results[i])
+            }
+            this.map.setCenter(results[0].geometry.location)
+          }
         }
-         const callbackPlace = (result, status) => {
-          console.log(result);
-          this.listItems.push(result);
+        this.service.nearbySearch(request, callback)
+
+        const callbackPlace = (result, status) => {
+          this.listItems.push(result)
         }
       } catch (error) {
-        console.log(error)
+        console.error(error)
       }
-      // function createMarker(place) {
-      //   console.log(place)
-      //   google.maps.Marker({
-      //     position: place.geometry.location,
-      //     map: map
-      //   })
-      // }
+      const createMarker = (place) => {
+        if (!place.geometry || !place.geometry.location) return
+
+        this.map.addListener('click', (mapsMouseEvent) => {
+            // Close the current InfoWindow.
+            
+            const {lat, lng} = mapsMouseEvent.latLng.toJSON();
+            console.log(lat, lng);
+            this.myLatLng.lat = lat;
+            this.myLatLng.lng = lng;
+            console.log(this.myLatLng)
+            this.infoWindow.close()
+            // Create a new InfoWindow.
+            this.infoWindow = new google.maps.InfoWindow({
+              position: mapsMouseEvent.latLng
+            })
+            this.infoWindow.setContent(JSON.stringify(mapsMouseEvent.latLng.toJSON(), null, 2))
+            this.infoWindow.open(this.map)
+          })
+      }
+    },
+    mounted() {
+      
     }
   }
 }
 </script>
 <template>
   <div class="about">
-    <h1>Get Business</h1>
-    <button class="business-btn" @click="getData">Get Data</button>
-    <div>
-      <div>
-        <div v-for="item in listItems">
-        <ul class="business">
-          <li>Nombre{{ item.name }}</li>
-          <li>Estatus {{ item.business_status }}</li>
-          <li>Sitio web: <a src='{{item.website}}'>{{ item.website }}</a></li>
-          <li>Tipo: <span v-for="item in item.types">, {{ item }}</span>.</li>
-          <li>Telefono: {{ item.formatted_phone_number }}</li>
-        </ul>
-        </div>
-      </div>
-      <div id="map"></div>
-      <!-- Rest of your template -->
+    <header>
+      <h1>Business at Google Maps</h1>
+      <button class="business-btn" @click="getData">Get Data</button>
+    </header>
+    <div id="map"></div>
+    <div class="business-list-container">
+      <ul class="business" v-for="item in listItems">
+        <li><span style="font-weight: bold">Nombre: </span>{{ item.name }}</li>
+        <li><span style="font-weight: bold">Estatus: </span>{{ item.business_status }}</li>
+        <li>
+          <span style="font-weight: bold">Sitio web: </span
+          ><a class="website-link" href="{{ item.website }}" target="blank_">{{ item.website }}</a>
+        </li>
+        <li>
+          <span style="font-weight: bold">Tipo: </span
+          ><span v-for="item in item.types">, {{ item }}</span
+          >.
+        </li>
+        <li>
+          <span style="font-weight: bold">Telefono: </span
+          ><a href="tel:{{ item.formatted_phone_number }}">{{ item.formatted_phone_number }}</a>
+        </li>
+      </ul>
     </div>
   </div>
 </template>
 
 <style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-    flex-direction: column;
-  }
-  .business-btn {
-    padding: 10px 20px;
-    border-radius: 10px;
-    background-color: rgb(65, 218, 218);
-  }
-  .business {
-    border: 2px solid rgb(255, 166, 0);
-    border-radius: 5px;
-    margin: 10px 0;
-  }
+#map {
+  height: 500px;
+  max-width: 500px;
+  margin: auto;
+}
+.about {
+  margin: 0 auto;
+}
+.about header {
+  text-align: center;
+}
+.business-btn {
+  padding: 10px 20px;
+  margin: 20px 0;
+  border-radius: 10px;
+  background-color: #32bf84;
+  cursor: pointer;
+  box-shadow: rgba(50, 50, 93, 0.25) 0px 6px 12px -2px, rgba(0, 0, 0, 0.3) 0px 3px 7px -3px;
+  border: none;
+  font-size: large;
+  color: white;
+  font-weight: bold;
+}
+
+.business-list-container {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+}
+.business {
+  box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
+  border-radius: 5px;
+  margin: 10px;
+  padding: 10px;
+  list-style-type: none;
+  width: 400px;
+  text-align: left;
+  overflow: hidden;
+}
+.website-link {
+  cursor: pointer;
 }
 </style>
