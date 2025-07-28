8e9f68ba3622c436ec3a5c5227f3f441239fbfa9
rene-mdd <rene.rrrg@gmail.com>
2025-07-28 22:06:14 +0200

    new vite config

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/rene-mdd.github.io/',
+  base: '/business-map/',
   plugins: [
     vue(),
     vueJsx(),
